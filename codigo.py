09f321e71f569d41e544718a1706db0d7f1e18ac
JamidIsrael39 <israel.93.ig@outlook.com>
2024-11-07 20:42:17 -0600

    Revert "Codigo para realizar un nuevo PR"
    This reverts commit 47f0352fe9c0a832fa0a3fead8e1422495497ccb.

--- codigo.py
+++ codigo.py
@@ -4,8 +4,6 @@ apellido = "Saenz"
 print("Hola " + nombre + " " + apellido + "¿Como estas?")
 print("Bolo, puerca y terry")
 
-print("Esto es uan modificacion para hacer un pull-request")
 print("Prueba para PR")
 
 lista3 = ["bolo", "puerca", "terry"]
-
