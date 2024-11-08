09f4e4e192a5939cde2e3a06cc5b4fe9c33527e9
JamidIsrael39 <israel.93.ig@outlook.com>
2024-11-07 20:33:16 -0600

    Revert "Agregando lista-diccionario vacios"
    This reverts commit 94303bc94a76ef8445436df3a2a80c3864ac4352.

--- codigo.py
+++ codigo.py
@@ -6,6 +6,6 @@ print("Bolo, puerca y terry")
 
 print("Esto es uan modificacion para hacer un pull-request")
 print("Prueba para PR")
-lista = []
-diccionario = {}
+
 lista3 = ["bolo", "puerca", "terry"]
+
