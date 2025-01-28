973e1d202c28ce1c3297e1cec817aba4223c05a9
JamidIsrael39 <israel.93.ig@outlook.com>
2025-01-27 20:30:27 -0600

    Agregando suma, maximo y quicksorting

--- GrokkingCode/recursion.py
+++ GrokkingCode/recursion.py
@@ -11,6 +11,46 @@ def fact(x):
 	else: 
 		return x * fact(x -1) 
 
+def sum(lista):
+	if lista == []:
+		return 0
+	else:
+		return lista[0] + sum(lista[1:])
+
+def cuenta(lista):
+	if lista == []:
+		return 0
+	else:
+		return 1 + cuenta(lista[1:])
+
+def max(lst):
+    if len(lst) == 0:
+        return None
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        max_num = max(lst[1:])
+        return lst[0] if lst[0] > max_num else max_num
+
+def quicksort(array):
+  if len(array) < 2:
+    # base case, arrays with 0 or 1 element are already "sorted"
+    return array
+  else:
+    # recursive case
+    pivot = array[0]
+    # sub-array of all the elements less than the pivot
+    less = [i for i in array[1:] if i <= pivot]
+    # sub-array of all the elements greater than the pivot
+    greater = [i for i in array[1:] if i > pivot]
+    return quicksort(less) + [pivot] + quicksort(greater)
+
 if __name__ == "__main__": 
-	countdown(5)
-	print(fact(4))
+	#countdown(5)
+	#print(fact(4))
+	arreglo = [1,20,3,4,5]
+	print(arreglo)
+	print(sum(arreglo))
+	print(cuenta(arreglo))
+	print(max(arreglo))
+	print(quicksort(arreglo))
