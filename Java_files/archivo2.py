b04c3056c841baabb086cf98006b6481c98c5205
JamidIsrael39 <israel.93.ig@outlook.com>
2025-03-10 18:48:28 -0600

    Dejando de nuevo 3 lineas en archivo2

--- Java_files/archivo2.py
+++ Java_files/archivo2.py
@@ -1,8 +1,3 @@
 print("Esta es la linea 1")
 print("Esta es la linea 2")
 print("Esta es la linea 3")
-
-for i in range(0, 5)
-	print(i)
-print("Linea despues del regreso del archivo")
-print("Esta es linea 4")
