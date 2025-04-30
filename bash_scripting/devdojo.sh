44a3b1c4626a495de82c70fc192f660a8a96a678
JamidIsrael39 <israel.93.ig@outlook.com>
2025-04-29 23:30:58 -0600

    Agregando cambios al archivo devdojo.sh

--- bash_scripting/devdojo.sh
+++ bash_scripting/devdojo.sh
@@ -5,3 +5,11 @@ name="Devdojo"
 echo "Hola mundo"
 echo "Hola ${name}"
 echo "Hi there" $1
+
+#echo "What is your name?"
+#read name
+#La siguiente linea es una reduccion de los dos anteriores
+read -p "What is your name? " name
+
+echo "Hi there ${name}"
+echo "Welcome to Devdojo!"
