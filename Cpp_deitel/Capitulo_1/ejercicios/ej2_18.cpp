1358a08bcfdad3a73009b814903452f1b50f5b42
JamidIsrael39 <israel.93.ig@outlook.com>
2025-05-28 23:09:06 -0600

    Agregando ejercicio 18 de deitel c++

--- Cpp_deitel/Capitulo_1/ejercicios/ej2_18.cpp
+++ Cpp_deitel/Capitulo_1/ejercicios/ej2_18.cpp
@@ -6,7 +6,22 @@ using std::endl;
 
 int main() {
 
-	cout << "12\t34\n";
+	int numero_1{0}, numero_2{0};
+
+	cout << "Ingrese dos numeros: \n";
+	cin >> numero_1 >> numero_2;
 	
+	if (numero_1 > numero_2){
+		cout << numero_1 << " es mayor " << numero_2 << "\n"  << endl;
+	}
+	
+	if (numero_2 > numero_1){
+		cout << numero_2 << " es mayor " << numero_1 << "\n" << endl;
+	}
+	
+	if (numero_1 == numero_2){
+		cout << "Estos números son iguales \n";
+	}
+
 	return 0;
 }
