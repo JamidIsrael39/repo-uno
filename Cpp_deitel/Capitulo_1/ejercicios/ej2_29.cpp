40e2fbda92139f00779b79d53bc4a7dea714a86f
jamidgiron-desktop <israel.93.ig@outlook.com>
2025-06-09 20:07:16 -0600

    Agregando ejercicio 2.29

---
+++ Cpp_deitel/Capitulo_1/ejercicios/ej2_29.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+
+	cout << "Programa que genera e imprime una tabla en consola con los \ncuadrados y cubos del 0 al 10" << endl;
+	cout << "numero\t cuadrado\t cubo" << endl;
+	cout << "0\t " << 0 * 0 << " \t \t " << 0 * 0 * 0  << endl;
+	cout << "1\t " << 1 * 1 << " \t \t " << 1 * 1 * 1  << endl;
+	cout << "2\t " << 2 * 2 << " \t \t " << 2 * 2 * 2  << endl;
+	cout << "3\t " << 3 * 3 << " \t \t " << 3 * 3 * 3  << endl;
+	cout << "4\t " << 4 * 4 << " \t \t " << 4 * 4 * 4  << endl;
+	cout << "5\t " << 5 * 5 << " \t \t " << 5 * 5 * 5  << endl;
+	cout << "6\t " << 6 * 6 << " \t \t " << 6 * 6 * 6  << endl;
+	cout << "7\t " << 7 * 7 << " \t \t " << 7 * 7 * 7  << endl;
+	cout << "8\t " << 8 * 8 << " \t \t " << 8 * 8 * 8  << endl;
+	cout << "9\t " << 9 * 9 << " \t \t " << 9 * 9 * 9  << endl;
+	cout << "10\t " << 10 * 10 << " \t \t " << 10 * 10 * 10  << endl;
+}
